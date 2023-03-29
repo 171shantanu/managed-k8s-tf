a18326b01b676bd4084106f991fe0b1e63c22171
shantanu <shantanu171@gmail.com>
2023-03-29 11:26:26 +0530

    Added providers.tf

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source         = "hashicorp/aws"
+      versionversion = ">=4.59"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "vscode-personal"
+
+  default_tags {
+    tags = {
+      managed_by = "Terraform Managed resource"
+      project    = "Self Managed K8s"
+    }
+  }
+}
