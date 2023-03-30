7847f1d3f3bb5a3b3aa37ff0341b4efab83e2779
shantanu <shantanu171@gmail.com>
2023-03-30 19:58:01 +0530

    Added lifecycle rule for kep pair

--- aws/instances.tf
+++ aws/instances.tf
@@ -24,20 +24,28 @@ data "aws_ami" "ubuntu" {
 resource "aws_key_pair" "k8s_key" {
   key_name   = "self-k8s-key"
   public_key = tls_private_key.rsa_k8s.public_key_openssh
-
   depends_on = [tls_private_key.rsa_k8s]
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # Generating a rsa key for the ec2 instances to get the public key
 resource "tls_private_key" "rsa_k8s" {
   algorithm = "RSA"
   rsa_bits  = 4096
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # Strping the private key in the local using a local file resource block
 resource "local_file" "k8s_key_private" {
   content  = tls_private_key.rsa_k8s.private_key_pem
   filename = "k8s-key.pem"
+  lifecycle {
+    prevent_destroy = trues
+  }
 }
 
 # Resource block for the AWS EC2 Instance.
