7b607271bf3bec6ee50268db6778d839757a8500
shantanu <shantanu171@gmail.com>
2023-04-01 01:38:36 +0530

    Added sg.tf/ Added htttp provider

---
+++ aws/securirty-groups.tf
@@ -0,0 +1,33 @@
+# Data block for my IPv4
+data "http" "my_ip" {
+  url = "http://ipv4.icanhazip.com/"
+}
+
+# Resource block for the SG
+resource "aws_security_group" "public_sg" {
+  name        = "Public_SG"
+  description = "Allows Acces on PORT 22,80 & 443"
+  vpc_id      = aws_vpc.k8s_vpc.id
+
+  ingress = {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]
+    description = "Allowing SSH access from my computer"
+  }
+  egress = [{
+    cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]
+    description = "Allowing SSH access from my computer"
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+  }]
+
+  tags = {
+    "Name"  = "${local.name_suffix}-Public-SG"
+    "Ports" = "22, 80, 443"
+  }
+}
+
+# Resource block for the SG Rule for port 22 Ingress 
--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">=4.59"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = ">=3.2.1"
+    }
   }
 }
 
@@ -11,6 +15,15 @@ provider "aws" {
   region  = "us-east-1"
   profile = "vscode-personal"
 
+  default_tags {
+    tags = {
+      managed_by = "Terraform Managed resource"
+      project    = "Self Managed K8s"
+    }
+  }
+}
+
+provider "http" {
   default_tags {
     tags = {
       managed_by = "Terraform Managed resource"
