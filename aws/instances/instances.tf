56be20074edbc4edffde7a85f50ce3f39c3ccafb
shantanu <shantanu171@gmail.com>
2023-03-31 22:17:39 +0530

    Added conditional expression for the AZ data

--- aws/instances/instances.tf
+++ aws/instances/instances.tf
@@ -48,22 +48,35 @@ resource "aws_ebs_volume" "master_node_ebs_volume" {
     "Size" = "10"
   }
 }
-s
+
+# Attaching EBS Volume to the Master Node 
 resource "aws_volume_attachment" "master_node_volume_attachment" {
   device_name = "/dev/sda1"
-  volume_id = aws_ebs_volume.master_node_ebs_volume.id
+  volume_id   = aws_ebs_volume.master_node_ebs_volume.id
   instance_id = aws_instance.k8s_master_node.id
 }
 
 # Resource block for the AWS EC2 Instances. (Worker Nodes)
 resource "aws_instance" "k8s_worker_nodes" {
-  count         = 2
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.ec2_instance_type
-  key_name      = aws_key_pair.k8s_key.key_name
+  count             = 2
+  ami               = data.aws_ami.ubuntu.id
+  instance_type     = var.ec2_instance_type
+  key_name          = aws_key_pair.k8s_key.key_name
+  availability_zone = count.index % 2 == 0 ? data.aws_availability_zones.az.names[0] : data.aws_availability_zones.az.names[1]
 
   tags = {
-    "Name" = "K8s-worker-node-${count.index}"
+    "Name" = "K8s-worker-node-${count.index + 1}"
   }
 }
 
+# Resource block for the EBS volume of the Worker Node Instance.
+resource "aws_ebs_volume" "worker_node_ebs_volume" {
+  availability_zone = count.index % 2 == 0 ? data.aws_availability_zones.az.names[0] : data.aws_availability_zones.az.names[1]
+  type              = "gp2"
+  count             = 2
+  size              = 10
+  tags = {
+    "Name" = "K8s-worker-node-ebs-volume"
+    "Size" = "10"
+  }
+}
