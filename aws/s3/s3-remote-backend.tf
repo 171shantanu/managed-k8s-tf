6cabf82d59403d4344e5ab48bd8fc4ad5db4f0c8
shantanu <shantanu171@gmail.com>
2023-04-15 10:01:09 +0530

    enabled acl adn versioning on tf bacckend bucket

--- aws/s3/s3-remote-backend.tf
+++ aws/s3/s3-remote-backend.tf
@@ -1,4 +1,23 @@
 # Creating an s3 bucket for the tf backend
 resource "aws_s3_bucket" "tf_backend" {
+  bucket = "tf-remote-backend"
+
+  tags = {
+    "Name" = "tf-remote-backend"
+  }
+}
+
+# making the bucket private 
+resource "aws_s3_bucket_acl" "tf_backend_acl" {
+  bucket = aws_s3_bucket.tf_backend.id
+  acl    = "private"
+}
+
+# enabling versioning for the bucket
+resource "aws_s3_bucket_versioning" "tf_backend_versioning" {
+  bucket = aws_s3_bucket.tf_backend.id
+  versioning_configuration {
+    status = "enabled"
+  }
 
 }
