ba1019ff4e4370b694445036dd039a2a2d60d58e
shantanu <shantanu171@gmail.com>
2023-04-15 11:41:53 +0530

    Added server side encryption

--- aws/s3/s3-remote-backend.tf
+++ aws/s3/s3-remote-backend.tf
@@ -17,7 +17,16 @@ resource "aws_s3_bucket_acl" "tf_backend_acl" {
 resource "aws_s3_bucket_versioning" "tf_backend_versioning" {
   bucket = aws_s3_bucket.tf_backend.id
   versioning_configuration {
-    status = "enabled"
+    status = "Enabled"
   }
+}
 
+# Applying server side configuration to the bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf-tf_backend_encryption" {
+  bucket = aws_s3_bucket.tf_backend.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
